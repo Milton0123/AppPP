faff07372e047d96838bf2b2b044bf316db4ca43
AgustinElizalde <agustinelizalde93@gmail.com>
2023-07-30 02:01:27 -0300

    Se agrego las validaciones de usuario

--- app/src/main/java/com/example/apppp/login_activity.kt
+++ app/src/main/java/com/example/apppp/login_activity.kt
@@ -1,8 +1,12 @@
 package com.example.apppp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.apppp.databinding.ActivityLoginBinding
+import com.example.apppp.ui.sale_activity
+
 
 class login_activity : AppCompatActivity() {
 
@@ -12,7 +16,29 @@ class login_activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginBtnRedirect.setOnClickListener{verificacion_de_campos()}
+
     }
 
+    fun verificacion_de_campos(){
+        if(binding.loginTextUser.text.toString().length == 0 ){
+            Toast.makeText(this, "El campo de usuario no debe de estar vacío",Toast.LENGTH_SHORT).show()
+        }
+        if(binding.loginTextPassword.text.toString().length == 0){
+            Toast.makeText(this, "El campo de contraseña no debe de estar vacío", Toast.LENGTH_SHORT).show()
+        }
+        if(binding.loginTextUser.text.toString() == users.empleados.name &&
+            binding.loginTextPassword.text.toString() == users.empleados.password){
 
+            val intent=Intent(this, employee_activity::class.java)
+            startActivity(intent)
+        }
+        if(binding.loginTextUser.text.toString() == users.admin.name &&
+            binding.loginTextPassword.text.toString() == users.admin.password){
+
+            val intent2=Intent(this, sale_activity::class.java)
+            startActivity(intent2)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/apppp/users.kt
@@ -0,0 +1,6 @@
+package com.example.apppp
+
+object users {
+    var empleados: employees = employees("pepe","pepe123")
+    var admin: employees = employees("juan", "juan123")
+}
