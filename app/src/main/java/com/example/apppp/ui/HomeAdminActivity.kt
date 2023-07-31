a8dd82fd13195d90a9d6de1c8f14fd473bac8d6f
Milton0123 <maletta.milton@gmail.com>
2023-07-31 09:08:53 -0300

    create pantalla home admin

--- app/src/main/java/com/example/apppp/ui/HomeAdminActivity.kt
+++ app/src/main/java/com/example/apppp/ui/HomeAdminActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.apppp.R
 
-class MainActivity : AppCompatActivity() {
+class HomeAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_activity)
     }
 }
