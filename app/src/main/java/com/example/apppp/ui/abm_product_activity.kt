480defda0ffcb3cfe64e2e17db5a8528a4c03a79
AgustinElizalde <agustinelizalde93@gmail.com>
2023-08-13 20:15:22 -0300

    Se agrego la opcion de baja de producto

--- app/src/main/java/com/example/apppp/ui/abm_product_activity.kt
+++ app/src/main/java/com/example/apppp/ui/abm_product_activity.kt
@@ -19,6 +19,7 @@ class abm_product_activity : AppCompatActivity() {
 
         binding.crudProductIvBack.setOnClickListener { volver() }
         binding.crudBtAdd.setOnClickListener { add() }
+        binding.crudBtRemove.setOnClickListener { delete_product() }
     }
 
     fun add(){
@@ -44,11 +45,44 @@ class abm_product_activity : AppCompatActivity() {
 
     }
 
+    fun delete_product(){
+        val product_id = binding.crudEdIdProduct.text.toString().toIntOrNull()
+
+        if (product_id != null) {
+            delete(product_id)
+        } else {
+            Toast.makeText(this, "ID de producto inválido", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun delete(id: Int){
+
+        val product_delete = ProductBD.list.find {it.id == id}
+
+        if (product_delete != null){
+            ProductBD.list.remove(product_delete)
+            Toast.makeText(this, "Producto eliminado correctamente", Toast.LENGTH_SHORT).show()
+
+            println("Contenido de la lista después de eliminar el producto:")
+            for (product in ProductBD.list)
+                println(product)
+
+            clear_id()
+        }else{
+            Toast.makeText(this, "No se ha encontrado el producto, ID invalido",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
     fun clear(){
         binding.crudEtIdProveedor.text.clear()
         binding.crudEtNameProduct.text.clear()
         binding.crudEtPriceProduct.text.clear()
         binding.crudEtQuantityProduct.text.clear()
+    }
+
+    fun clear_id(){
+        binding.crudEdIdProduct.text.clear()
     }
 
     fun volver(){
