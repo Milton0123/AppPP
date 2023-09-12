90368c63cc93e6765948563dd2c4554dc42f99d2
Milton0123 <maletta.milton@gmail.com>
2023-09-12 11:42:38 -0300

    create splash and login

---
+++ app/src/main/java/com/example/apppp/ui/SplashLogin.kt
@@ -0,0 +1,11 @@
+package com.example.apppp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apppp.R
+
+class SplashLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
