d4ee9bbefc5ddbc1f86632038ddfe643a594185d
Milton0123 <maletta.milton@gmail.com>
2023-09-15 11:54:31 -0300

    menu hamburguesa

---
+++ app/src/main/java/com/example/apppp/ui/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apppp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apppp.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
