ccd2a6db98d754c667feada6d6e9629971323851
Milton0123 <maletta.milton@gmail.com>
2023-10-05 13:27:38 -0300

    create splash screen + news+

--- app/src/main/java/com/example/apppp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/apppp/ui/NewsActivity.kt
@@ -1,10 +1,24 @@
 package com.example.apppp.ui
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.apppp.R
+import com.example.apppp.databinding.ActivityNewsBinding
+import com.google.android.material.navigation.NavigationView
 
-class NewsActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity(){
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toogle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
