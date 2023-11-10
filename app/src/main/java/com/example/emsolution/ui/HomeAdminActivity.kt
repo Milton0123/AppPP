54b76481dca5d08d14c6cbd07d877599df99f945
AgustinElizalde <agustinelizalde93@gmail.com>
2023-11-10 14:17:47 -0300

    Codigo unido

--- app/src/main/java/com/example/emsolution/ui/HomeAdminActivity.kt
+++ app/src/main/java/com/example/emsolution/ui/HomeAdminActivity.kt
@@ -10,14 +10,19 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.emsolution.R
+import com.example.emsolution.back.ProductData
 import com.example.emsolution.databinding.ActivityHomeAdminBinding
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 
 class HomeAdminActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityHomeAdminBinding
+    val list = mutableListOf<CarouselItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,23 @@ class HomeAdminActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
+
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        val db = FirebaseFirestore.getInstance()
+
+        val qCollection = db.collection("q")
+        val specificCollection = qCollection.document("R4QQ1Qbs9j8d8n7kOKvV").collection("22")
+
+        specificCollection.get().addOnSuccessListener { productosQuerySnapshot ->
+            for (productoDocument in productosQuerySnapshot.documents) {
+                val producto = productoDocument.toObject(ProductData::class.java)
+                val imageUrlString = producto?.image.toString()
+                list.add(CarouselItem(imageUrlString))
+                }
+
+            // Agrega las imágenes al carrousel
+            carousel.addData(list)
+        }
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
